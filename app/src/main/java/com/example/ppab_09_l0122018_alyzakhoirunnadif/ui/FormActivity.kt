c2d5810565fc7bfdedfac9f0b2f735589275352d
Aorus <alyzanadif04@gmail.com>
2024-05-15 20:16:04 +0700

    small fix

--- app/src/main/java/com/example/ppab_09_l0122018_alyzakhoirunnadif/ui/FormActivity.kt
+++ app/src/main/java/com/example/ppab_09_l0122018_alyzakhoirunnadif/ui/FormActivity.kt
@@ -26,7 +26,6 @@ class FormActivity : AppCompatActivity() {
 
         binding.etSplashArt.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
